ee2c61178545dd228a18b2be9efc47562162d349
WenquanZou <wz1816@ic.ac.uk>
2020-03-02 14:47:36 +0000

    Add tooltips for annotation

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,19 +1,37 @@
 import React, {Component} from 'react';
 import {List, ListItem, ListItemText, Divider} from '@material-ui/core';
+import HelpIcon from '@material-ui/icons/Help';
+import Tooltip from "@material-ui/core/Tooltip";
+import withStyles from "@material-ui/core/styles/withStyles";
+
+const LightTooltip = withStyles(theme => ({
+  tooltip: {
+    backgroundColor: theme.palette.common.white,
+    color: 'rgba(0, 0, 0, 0.87)',
+    boxShadow: theme.shadows[1],
+    fontSize: 14,
+  },
+}))(Tooltip);
 
 class Menu extends Component {
+
     render() {
         return (
-            <List className="scrollable">
-                {this.props.plays.map((play, key) => (
-                    <div key={key}>
-                        <ListItem button onClick={this.props.loadPlay(play)}>
-                            <ListItemText primary={play}/>
-                        </ListItem>
-                        <Divider/>
-                    </div>
-                ))}
-            </List>
+            <>
+                <LightTooltip title="Select text and right click to annotate, then click submit button" aria-label="submit annotation">
+                    <HelpIcon fontSize="large"/>
+                </LightTooltip>
+                <List className="scrollable">
+                    {this.props.plays.map((play, key) => (
+                        <div key={key}>
+                            <ListItem button onClick={this.props.loadPlay(play)}>
+                                <ListItemText primary={play}/>
+                            </ListItem>
+                            <Divider/>
+                        </div>
+                    ))}
+                </List>
+            </>
         );
     }
 }
